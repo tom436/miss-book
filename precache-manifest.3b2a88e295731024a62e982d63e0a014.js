d2f85c668db7db68dd43be383841d2b892115592
tom temkin <tomtemkin14@gmail.com>
2020-06-21 09:49:11 +0300

    deploy

---
+++ precache-manifest.3b2a88e295731024a62e982d63e0a014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f851b848346f7deec3",
+    "url": "/miss-book/css/app.7799f0f3.css"
+  },
+  {
+    "revision": "dcae9692066995bf097bf8ad1d92ffd3",
+    "url": "/miss-book/index.html"
+  },
+  {
+    "revision": "d1f851b848346f7deec3",
+    "url": "/miss-book/js/app.858572b4.js"
+  },
+  {
+    "revision": "66d14d5eaf1575bfc065",
+    "url": "/miss-book/js/chunk-vendors.5ddb895d.js"
+  },
+  {
+    "revision": "f2660540af2f9d0e8bd9d3aa279d39a8",
+    "url": "/miss-book/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/miss-book/robots.txt"
+  }
+]);
